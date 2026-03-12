248ac2d6275174864aec9c869f4a07b3e511fca3
Gerardo6741 <759369@student.dusd.net>
2026-03-12 15:24:48 -0700

    vvv
    vvb

--- P7PlayerControlTellez/P7BasicGamePlayTellez/Assets/SpawnManager.cs
+++ P7PlayerControlTellez/P7BasicGamePlayTellez/Assets/SpawnManager.cs
@@ -1,8 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+
+    // Start is called before the first frame update
     void Start()
     {
         
@@ -11,6 +17,12 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        }
     }
 }

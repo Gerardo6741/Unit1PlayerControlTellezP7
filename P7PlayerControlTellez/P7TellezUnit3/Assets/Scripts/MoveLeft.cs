d0dc5a4cf339c76216de6a23caca3237c69a3d80
Gerardo6741 <759369@student.dusd.net>
2026-03-26 15:25:18 -0700

    hh
    hhh

--- P7PlayerControlTellez/P7TellezUnit3/Assets/Scripts/MoveLeft.cs
+++ P7PlayerControlTellez/P7TellezUnit3/Assets/Scripts/MoveLeft.cs
@@ -4,14 +4,17 @@ public class MoveLeft : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private float speed = 30;
+    private PlayerController playerControllerScript;
     void Start()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);  
+        playerControllerScript =
+            GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+
     }
 }
